a2334752ada146b5c1bc4f8af864281ed1c87f65
thenepaligamer <thenepaligamer@tng.com>
2022-09-20 11:50:44 +0545

    upd: changed database from mysql to postgresql

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -168,3 +168,12 @@ class VolunteerController extends Controller
         ],201);
     }
 }
+
+/*
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=dms
+DB_USERNAME=root
+DB_PASSWORD=
+*/
