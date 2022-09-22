bf3204fc38655f3eeabb4d774b0290ccc2aef43d
thenepaligamer <thenepaligamer@tng.com>
2022-09-22 18:12:09 +0545

    feat: updating status for events pt22

--- app/Http/Controllers/UserConfigController.php
+++ app/Http/Controllers/UserConfigController.php
@@ -106,3 +106,130 @@ class UserConfigController extends Controller
         ],201);
     }
 }
+
+
+/*
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feedback;
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $showAllDetails = Feedback::all();
+        return response()->json($showAllDetails);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'phone' => 'required',
+            'message' => 'required'
+        ]);
+
+        $feedbackDetails = [
+            'phone' => $data['phone'],
+            'message' => $data['message']
+        ];
+
+        $feedbacks = Feedback::create($feedbackDetails);
+
+        return response()->json([
+            'message' => 'Added feedback',
+            'relief details' => $feedbacks
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Feedback  $feedback
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Feedback $feedback)
+    {
+        $findFeedbackToDelete = Feeback::find($id);
+        $findFeedbackToDelete->delete();
+
+        return response()->json([
+            "message" => "Feedback deleted",
+        ],201);
+    }
+}
+
+*/
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'phone',
+        'message'
+    ];
+}
