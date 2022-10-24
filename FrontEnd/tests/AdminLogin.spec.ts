7e1108085bea0baeb8639897901693522d6572c3
sachinshilwal <sachin.shilwal@dzangolab.com>
2022-10-24 17:03:45 +0545

    feat: test case to for admin login

---
+++ FrontEnd/tests/AdminLogin.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  await page.goto('http://localhost:3000/');
+
+  await page.goto('http://localhost:3000/admin');
+
+  await page.goto('http://localhost:3000/admin/login');
+
+  await page.locator('input[name="email"]').click();
+
+  await page.locator('input[name="email"]').fill('sachin@gmail.com');
+
+  await page.locator('input[name="email"]').press('Tab');
+
+  await page.locator('input[name="password"]').fill('@Sachin123');
+
+  await page.locator('input[name="password"]').press('Enter');
+  await expect(page).toHaveURL('http://localhost:3000/admin');
+
+});
