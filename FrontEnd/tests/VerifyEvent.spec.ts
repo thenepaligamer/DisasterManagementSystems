ec42bc6a88d70ac8bdd43d1d3da37e8b18167b66
sachinshilwal <sachin.shilwal@dzangolab.com>
2022-10-24 22:07:27 +0545

    feat: test verify events

---
+++ FrontEnd/tests/VerifyEvent.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify Events as Admin', async ({ page }) => {
+
+  await page.goto('http://localhost:3000/');
+
+  await page.goto('http://localhost:3000/admin');
+
+  await page.goto('http://localhost:3000/admin/login');
+
+  await page.locator('input[name="email"]').click();
+
+  await page.locator('input[name="email"]').fill('sachin@gmail.com');
+
+  await page.locator('input[name="email"]').press('Tab');
+
+  await page.locator('input[name="password"]').fill('@sachin123');
+
+  await page.locator('input[name="password"]').press('Enter');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.locator('input[name="password"]').click();
+
+  await page.locator('input[name="password"]').press('Control+Shift+I');
+
+  await page.locator('input[name="password"]').fill('@Sachin123');
+
+  await page.locator('input[name="password"]').press('Enter');
+  await expect(page).toHaveURL('http://localhost:3000/admin');
+
+  await page.getByRole('button', { name: 'Incidents' }).click();
+
+  await page.getByRole('link', { name: 'Pending Incidents' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/admin/pending-events');
+
+  await page.locator('.px-6 > .font-medium').first().click();
+
+  await page.locator('.px-6 > .font-medium').first().click();
+
+});
